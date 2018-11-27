f411c1f556de59360602c517abe1feb79e46fdd3
nmwei <254060001@qq.com>
2018-11-27 22:47:50 +0800

    开发调试

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: __dirname,
     filename: './release/bundle.js' 
   },
+  devtool: 'eval-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
