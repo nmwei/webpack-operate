f1aab7760383ecbcb3ef5d7d4cf953e722891f0f
nmwei <254060001@qq.com>
2018-11-27 22:53:59 +0800

    ES7提案-装饰器

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -31,7 +31,7 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ['env', "stage-0"],
-          plugins: []
+          plugins: ["transform-decorators-legacy"]
         }
       }
     }]
