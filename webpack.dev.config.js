e56eb9f832a57364d30809dc5e851f789fffabde
nmwei <254060001@qq.com>
2018-11-27 22:46:24 +0800

    模块热更新

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,19 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname,
     filename: './release/bundle.js' 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    open: true, //浏览器自动打开
+    port: 9000
   }
 }
